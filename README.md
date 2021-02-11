6ae592d22996cdb771e43b5233dc8b8cc62f83a1
Robin Simonklein <rsimonklein@gmail.com>
2021-02-11 13:10:38 +0100

    Create Perceptron class

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # gobelins-workshop-ia
+
+## Sans installer le projet
+```shell
+node app.js
+```
+
+## En installant le projet
+```shell
+npm install
+```
+```shell
+npm run start
+```
---
+++ app.js
@@ -0,0 +1,41 @@
+import Perceptron from "./Perceptron.js";
+
+function shuffle(array) {
+    return [...array].sort(() => Math.random() - 0.5);
+}
+
+const myPerceptron = new Perceptron(2)
+const dataset = [
+    [[20, 5], [-1]],
+    [[18, 25], [-1]],
+    [[24, 15], [-1]],
+    [[22, 25], [-1]],
+    [[21, 0], [-1]],
+    [[25, 15], [-1]],
+
+    [[5, 45], [1]],
+    [[9, 5], [1]],
+    [[12, 15], [1]],
+    [[7, 25], [1]],
+    [[8, 75], [1]],
+    [[15, 75], [1]],
+]
+
+console.log('before training prediction')
+dataset.forEach(data => {
+    console.log(`For ${data[0]} expected : ${data[1][0]} | obtain : ${myPerceptron.predict(data[0])}`)
+})
+console.log(myPerceptron.weights)
+
+for(let i = 0; i<500; i++) {
+    const ds = shuffle(dataset)
+    ds.forEach(data => {
+        myPerceptron.train(data[0], data[1][0])
+    })
+}
+
+console.log(myPerceptron.weights)
+console.log('after training prediction')
+dataset.forEach(data => {
+    console.log(`For ${data[0]} expected : ${data[1][0]} | obtain : ${myPerceptron.predict(data[0])}`)
+})
